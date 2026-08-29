128ff5a16213d626de28f5e9bddf1a765658ad50
agroindustriascayoblanco1-dev <agroindustriascayoblanco1@gmail.com>
2026-08-29 07:59:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const PERIODOS={
- q1:{nombre:"Quincena 1",mes:"Agosto 2026",archivo:"RECIBOS QUINCENA 1.pdf"},
- q2:{nombre:"Quincena 2",mes:"Julio 2026",archivo:"RECIBOS QUINCENA 2.pdf"}
+ q1:{nombre:"Quincena 1",mes:"Agosto del 11 de Julio al 25 de Julio del 2026",archivo:"RECIBOS QUINCENA 1.pdf"},
+ q2:{nombre:"Quincena 2",mes:"Agosto del 26 de Julio al 9 de Agosto del 2026",archivo:"RECIBOS QUINCENA 2.pdf"}
 };
 let empleadoActual=null,pdfActual=null,paginaEncontrada=null,quincenaActual=null;
 const $=id=>document.getElementById(id);
